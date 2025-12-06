798ee30ff805f8933ddbbe6f4b0e9c9cbe13a290
foodzone940-tech <foodzone940@gmail.com>
2025-12-06 20:09:42 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,29 +1,10 @@
 import express from "express";
-import {
-  addToCart,
-  getCart,
-  updateCart,
-  deleteCartItem,
-  clearCart
-} from "../controllers/cartController.js";
-
+import { sendMessage, getMessages } from "../controllers/chatController.js";
 import adminAuth from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
-// Add Product to Cart
-router.post("/add", adminAuth, addToCart);
-
-// Get User Cart
-router.get("/", adminAuth, getCart);
-
-// Update Cart Quantity
-router.put("/update/:id", adminAuth, updateCart);
-
-// Delete Single Cart Item
-router.delete("/delete/:id", adminAuth, deleteCartItem);
-
-// Clear Entire Cart
-router.delete("/clear", adminAuth, clearCart);
+router.post("/send", adminAuth, sendMessage);
+router.get("/:id", adminAuth, getMessages);
 
 export default router;
