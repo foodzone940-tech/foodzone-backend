def692ad96947b3d0b6930dea1927f6439584c63
foodzone940-tech <foodzone940@gmail.com>
2025-12-07 12:54:17 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,8 +8,8 @@ import {
     getAllUsers
 } from "../controllers/userController.js";
 
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { adminAuth } from "../middlewares/adminMiddleware.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import adminAuth from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
